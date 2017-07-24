f32f0e3e9e4b86611ba7d87b8ce2a694ca6ad06d
Szymon Krywus <szymon.krywus@MWPLKRAMACLT012.logthemin.com>
2017-07-24 15:05:40 +0200

    remove tokens

---
+++ server/_tokens.js
@@ -0,0 +1,3 @@
+export const SPACE = 'w9v8tyww8nf0';
+export const ACCESS_TOKEN = '1e346c90f5950976135d1cad468e1e2df9842eb9dfaf0d96cd0e2f66d7475ded';
+export const ACCESS_TOKEN_MANAGER = 'CFPAT-9723bfbf1d986dea0e03d9e22b417d8161c65238635094d57347e632c7bd52a0';
--- server/services/contentful.js
+++ server/services/contentful.js
@@ -1,13 +1,12 @@
 import {createClient} from 'contentful'
 import {createClient as createClientManager} from 'contentful-management';
-export const space = 'w9v8tyww8nf0';
-export const accessToken = '1e346c90f5950976135d1cad468e1e2df9842eb9dfaf0d96cd0e2f66d7475ded';
-export const accessTokenManager = 'CFPAT-9723bfbf1d986dea0e03d9e22b417d8161c65238635094d57347e632c7bd52a0';
+import {SPACE, ACCESS_TOKEN, ACCESS_TOKEN_MANAGER} from './../_tokens';
+
 
 export const contentfulClient = () => createClient({
-    accessToken,
-    space
+    ACCESS_TOKEN,
+    SPACE
 });
 export const contentfulManager = () => createClientManager({
-    accessToken: accessTokenManager
+    accessToken: ACCESS_TOKEN_MANAGER
 });
