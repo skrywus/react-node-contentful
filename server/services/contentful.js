b4db9e026b375515061549084b0a2dc3fffbab0a
Szymon Krywus <szymon.krywus@MWPLKRAMACLT012.logthemin.com>
2017-07-24 15:15:34 +0200

    fix

--- server/services/contentful.js
+++ server/services/contentful.js
@@ -2,10 +2,10 @@ import {createClient} from 'contentful'
 import {createClient as createClientManager} from 'contentful-management';
 import {SPACE, ACCESS_TOKEN, ACCESS_TOKEN_MANAGER} from './../_tokens';
 
-
+console.log(SPACE, ACCESS_TOKEN, ACCESS_TOKEN_MANAGER);
 export const contentfulClient = () => createClient({
-    ACCESS_TOKEN,
-    SPACE
+    accessToken: ACCESS_TOKEN,
+    space: SPACE
 });
 export const contentfulManager = () => createClientManager({
     accessToken: ACCESS_TOKEN_MANAGER
